ad669c32828fd18070613739487ec5ad7d5159ba
Bahadir Coskun <p.bahadircoskun@gmail.com>
2020-02-24 20:56:50 +0300

    fix bug on ray casting

--- Camera.cpp
+++ Camera.cpp
@@ -1,34 +1,38 @@
 #include "Camera.h"
 #include <string.h>
+#include <iostream>
 
 using namespace Eigen;
 
 Camera::Camera(int id,                      // Id of the camera
-        const char* imageName,       // Name of the output PPM file
-        const Vector3f& pos,         // Camera position
-        const Vector3f& gaze,        // Camera gaze direction
-        const Vector3f& up,          // Camera up direction
-        const ImagePlane& imgPlane)  // Image plane parameters
+		const char* imageName,       // Name of the output PPM file
+		const Vector3f& pos,         // Camera position
+		const Vector3f& gaze,        // Camera gaze direction
+		const Vector3f& up,          // Camera up direction
+		const ImagePlane& imgPlane)  // Image plane parameters
 {
-    this->id = id;
-    this->imgPlane = imgPlane;
-    strcpy(this->imageName, imageName);
-    this->gaze = gaze;
-    this->pos = pos;
-    this->up = up;
-    nxDivisionAvoided = (float)1 / imgPlane.nx;
-    nyDivisionAvoided = (float)1 / imgPlane.ny;
-    right = up.cross(gaze);
+	this->id = id;
+	this->imgPlane = imgPlane;
+	strcpy(this->imageName, imageName);
+
+	this->gaze = gaze.normalized();
+	Vector3f w = (-gaze).normalized();
+	right = (up.cross(w)).normalized();
+	this->up = w.cross(right);
+
+	this->pos = pos;
+	nxDivisionAvoided = (float)1.0f / imgPlane.nx;
+	nyDivisionAvoided = (float)1.0f / imgPlane.ny;
 }
 
 Ray Camera::getPrimaryRay(int col, int row) const
 {
-    float u = imgPlane.left + (imgPlane.right - imgPlane.left) * (col + 0.5f) * nxDivisionAvoided;
-    float v = imgPlane.top - (imgPlane.top - imgPlane.bottom) * (row + 0.5f) * nyDivisionAvoided;
-    Vector3f m = pos + gaze * imgPlane.distance;
-    m(0) += u;
-    m(1) += v;
-    Ray ray(pos, (m - pos) / ((m - pos).norm()));
-    return ray;
+	float u = imgPlane.left + (imgPlane.right - imgPlane.left) * (col + 0.5f) * nxDivisionAvoided;
+	float v = imgPlane.top - (imgPlane.top - imgPlane.bottom) * (row + 0.5f) * nyDivisionAvoided;
+	Vector3f m = pos + gaze * imgPlane.distance;
+	m += u * right;
+	m += v * up;
+	Ray ray(pos, (m - pos) / ((m - pos).norm()));
+	return ray;
 
 }
